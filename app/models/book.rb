97e27ea3e88f9c87a54f151b7b7251c52ef44516
hanother3 <hanazawa.s58.1219@gmail.com>
2023-01-30 11:11:47 +0900

    bookモデルのバリデーション

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,2 +1,9 @@
 class Book < ApplicationRecord
+
+  with_options presence: true do
+    validates :book_name, :author, :image_url, :publishd_date, :publisher
+    validates :book_review
+  end  
+
+  belongs_to :user
 end
