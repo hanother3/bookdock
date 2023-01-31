8a10d3b3fd6e5c771c99dcc8ec9d28884f56e11b
hanother3 <hanazawa.s58.1219@gmail.com>
2023-01-31 22:27:08 +0900

    楽天APIのテーブル保存を実装

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -3,9 +3,21 @@ class BooksController < ApplicationController
     @books = Book.all
   end
 
-  def new
+  def search
     @book = Book.new
+    @books = RakutenWebService::Books::Book.search(title: params[:keyword]) if params[:keyword].present?
+  rescue StandardError => e
+    redirect_to books_url, alert: e.message
   end
+
+  def new
+      @book = Book.new(isbn: params[:isbn], title: params[:title], author: params[:author], image_url: params[:image_url])
+      @books = RakutenWebService::Books::Book.search(title: params[:keyword]) if params[:keyword].present?
+    rescue StandardError => e
+      redirect_to books_url, alert: e.message
+  end
+
+
 
   def create
     @book = Book.new(book_params)
@@ -17,9 +29,12 @@ class BooksController < ApplicationController
   end
 
   private
+
+
   def book_params
     params.require(:book).permit(
-      :book_name, :author, :image_url, :publishd_date, :publisher, :read_status_id, :book_review
-  ).merge(user_id: current_user.id)
+      :title, :author, :image_url, :publishd_date, :publisher, :read_status_id, :book_review
+  ).merge(user_id: current_user.id, isbn: params[:isbn], title: params[:title],  author: params[:author], image_url: params[:image_url])
   end
+
 end
