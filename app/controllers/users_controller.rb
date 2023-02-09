67677e5489a1e02ec9cacfc6f65100730fea65ab
hanother3 <hanazawa.s58.1219@gmail.com>
2023-02-09 17:00:44 +0900

    マイページの遷移を修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,8 @@
 class UsersController < ApplicationController
   def show
-    @nickname = current_user.nickname
-    @books = current_user.books    
-    @old_books = current_user.old_books    
+    user = User.find(params[:id])
+    @nickname = user.nickname
+    @books = user.books    
+    @old_books = user.old_books    
   end  
 end
