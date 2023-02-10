76f422d37d1b9e144ab28adf09d5164b374ae009
hanother3 <hanazawa.s58.1219@gmail.com>
2023-02-10 16:28:30 +0900

    マイページの既読・未読表示を分ける

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,8 @@ class UsersController < ApplicationController
   def show
     user = User.find(params[:id])
     @nickname = user.nickname
-    @books = user.books    
+    @books_read = user.books.where(read_status_id: 2)
+    @books_unread = user.books.where(read_status_id: 3)
     @old_books = user.old_books    
   end  
 end
