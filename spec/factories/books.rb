5c3aa7f29a571df3ba13ae064365c8af27144a6b
hanother3 <hanazawa.s58.1219@gmail.com>
2023-02-11 17:57:21 +0900

    読書投稿のテストコード

--- spec/factories/books.rb
+++ spec/factories/books.rb
@@ -1,5 +1,13 @@
 FactoryBot.define do
   factory :book do
-    
+    isbn              {9784123456789}
+    title             {"THE SAMPLE"} 
+    author            {"著者"}
+    publisher         {"テスト社"}
+    image_url         {"https://thumbnail.image.rakuten.co.jp"}
+    item_url          {"https://books.rakuten.co.jp"}
+    read_status_id    { 2 }
+    book_review       {"test"}
+    association :user
   end
 end
--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -1,5 +1,38 @@
 require 'rails_helper'
 
 RSpec.describe Book, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @book = FactoryBot.build(:book)
+  end
+
+  describe '読書投稿登録' do
+    context '読書投稿できるとき' do
+      it '全ての項目が存在し、価格が適切であれば登録できる' do
+        expect(@book).to be_valid
+      end
+    end
+
+    context '読書投稿できないとき' do
+      it 'isbnが空では登録できない' do
+        @book.isbn = nil
+        @book.valid?
+        expect(@book.errors.full_messages).to include "Isbn can't be blank"
+      end
+      it 'read_status_idが空では登録できない' do
+        @book.read_status_id = '1'
+        @book.valid?
+        expect(@book.errors.full_messages).to include("Read status can't be blank")
+      end
+      it 'book_reviewが空では登録できない' do
+        @book.book_review = nil
+        @book.valid?
+        expect(@book.errors.full_messages).to include "Book review can't be blank"
+      end
+      it 'userが紐付いていないと登録できない' do
+        @book.user = nil
+        @book.valid?
+        expect(@book.errors.full_messages).to include('User must exist')
+      end
+    end
+  end
 end
