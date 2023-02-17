facff5c87d77ae70354cd8a97dfda94bf1880858
hanother3 <hanazawa.s58.1219@gmail.com>
2023-02-17 17:25:26 +0900

    いいね削除機能

--- app/models/book.rb
+++ app/models/book.rb
@@ -15,4 +15,8 @@ class Book < ApplicationRecord
 
   has_many :favorites, dependent: :destroy
   has_many :favorite_users, through: :favorites, source: :user
+
+  def favorited?(user_id)
+    favorites.where(user_id: user.id).exists?
+ end
 end
